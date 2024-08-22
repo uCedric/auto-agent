d8743fe7cbfb66fab4c4328499ae97198cdac5b4
Cedric Liu <cedricliu1011@gmail.com>
2024-08-23 01:37:40 +0800

    add_prompt_record_to_db

--- api/src/main/java/com/example/api/repository/PromptRepository.java
+++ api/src/main/java/com/example/api/repository/PromptRepository.java
@@ -1,18 +1,21 @@
 package com.example.api.repository;
 
 import java.util.UUID;
-
 import org.springframework.stereotype.Repository;
-
-import com.example.api.entity.promptEntity;
-
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import com.example.api.entity.promptEntity;
 
 @Repository
 public interface PromptRepository extends JpaRepository<promptEntity, UUID> {
 
-    @Query(value = "SELECT content FROM prompts p WHERE p.uuid = 'bff72df2-d3a6-4287-a22f-ddffccc69c97'", nativeQuery = true)
-    String findByPromptByUuid(@Param("uuid") UUID uuid);
+    @Transactional
+    @Modifying
+    @Query(value = "INSERT INTO prompts (uuid, user_uuid, content) VALUES (:uuid, :userUuid, :content)", nativeQuery = true)
+    int addPrompt(@Param("uuid") UUID uuid, @Param("userUuid") UUID userUuid,
+            @Param("content") String content);
 }
