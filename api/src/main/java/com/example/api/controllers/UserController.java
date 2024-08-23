1f8156bab7b6728491fc5176ab030d96ec98943f
Cedric Liu <85926514+uCedric@users.noreply.github.com>
2024-08-23 17:56:25 +0800

    Cedric/feat/add redis conntion initialization (#6)
    * add_redis_connection_initialization
    
    * add_todo_comment

--- api/src/main/java/com/example/api/controllers/UserController.java
+++ api/src/main/java/com/example/api/controllers/UserController.java
@@ -1,15 +1,22 @@
 package com.example.api.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.api.validators.AuthValidator;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     @GetMapping("/all")
-    public String getUser() {
+    public String getUser(@RequestHeader("Authorization") String token) {
+        // TODO: develop auth exception class
+        AuthValidator authValidator = new AuthValidator();
+        authValidator.validateToken(token);
+
         return "Hello User!";
     }
 
