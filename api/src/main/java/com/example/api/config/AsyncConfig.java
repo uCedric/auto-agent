4fd8cffb11b4fc59bf22bf0c52c1d3a0d9d5dd0f
Cedric Liu <cedricliu1011@gmail.com>
2024-08-29 01:21:30 +0800

    add new thread pool to handle async

--- api/src/main/java/com/example/api/config/AsyncConfig.java
+++ api/src/main/java/com/example/api/config/AsyncConfig.java
@@ -8,12 +8,25 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 public class AsyncConfig {
 
     @Bean(name = "dbAsyncExecutor")
-    public ThreadPoolTaskExecutor taskExecutor() {
+    public ThreadPoolTaskExecutor dbAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(2);
-        executor.setMaxPoolSize(2);
-        executor.setQueueCapacity(500);
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(100);
         executor.setThreadNamePrefix("DbAsyncThread-");
+        // executor.setRejectedExecutionHandler((r, executor1) -> log.warn("Task
+        // rejected, thread pool is full and queue is also full"));
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean(name = "AsyncProcessorExecutor")
+    public ThreadPoolTaskExecutor AsyncProcessorExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(3);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("AsyncProcessorThread-");
         // executor.setRejectedExecutionHandler((r, executor1) -> log.warn("Task
         // rejected, thread pool is full and queue is also full"));
         executor.initialize();
