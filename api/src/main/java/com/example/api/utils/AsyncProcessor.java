2c68eef15561adfac5847843f6ecb54b9de69be6
Cedric Liu <cedricliu1011@gmail.com>
2024-09-05 02:42:12 +0800

    refact asyncProcessor and services logic

--- api/src/main/java/com/example/api/utils/AsyncProcessor.java
+++ api/src/main/java/com/example/api/utils/AsyncProcessor.java
@@ -3,6 +3,8 @@ package com.example.api.utils;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.concurrent.ExecutionException;
 import org.springframework.scheduling.annotation.Async;
@@ -10,7 +12,7 @@ import org.springframework.scheduling.annotation.Async;
 public class AsyncProcessor<T, R> {
     private List<Function<T, CompletableFuture<R>>> services = new ArrayList<>();
 
-    private List<T> dtos = new ArrayList<>();
+    private T dto;
 
     public static <T, R> AsyncProcessor<T, R> init() {
         AsyncProcessor<T, R> asyncProcessor = new AsyncProcessor<>();
@@ -19,23 +21,23 @@ public class AsyncProcessor<T, R> {
 
     public AsyncProcessor<T, R> addTask(Function<T, CompletableFuture<R>> service, T dto) {
         this.services.add(service);
-        this.dtos.add(dto);
+        this.dto = dto;
         return this;
     }
 
     @Async("AsyncProcessorExecutor")
-    public CompletableFuture<List<R>> process() throws InterruptedException, ExecutionException {
-        List<R> results = new ArrayList<>();
+    public CompletableFuture<Map<String, R>> process() throws InterruptedException, ExecutionException {
+        // List<R> results = new ArrayList<>();
+        Map<String, R> results = new HashMap<>();
+        int order = 1;
 
         for (Function<T, CompletableFuture<R>> service : services) {
-            for (T dto : dtos) {
-                CompletableFuture<R> taskResult = service.apply(dto);
-                R result = taskResult.get();
-                results.add(result);
+            CompletableFuture<R> taskResult = service.apply(dto);
+            R result = taskResult.get();
 
-                continue;
-            }
-            continue;
+            results.put("task" + order, result);
+
+            order++;
         }
 
         return CompletableFuture.completedFuture(results);
