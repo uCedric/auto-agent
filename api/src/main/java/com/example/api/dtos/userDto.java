df72131ccd84cba1b599437f9d3127a471b3fbe7
Cedric Liu <cedricliu1011@gmail.com>
2024-09-27 11:49:24 +0800

    fix: add email regex format checker

--- api/src/main/java/com/example/api/dtos/userDto.java
+++ api/src/main/java/com/example/api/dtos/userDto.java
@@ -2,7 +2,7 @@ package com.example.api.dtos;
 
 import com.example.api.utils.constants;
 
-import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
@@ -10,7 +10,7 @@ import jakarta.validation.constraints.Size;
 public class userDto {
     @NotEmpty(message = constants.USER_EMAIL_CAN_NOT_EMPTY)
     @NotBlank(message = constants.USER_EMAIL_CAN_NOT_CONTAIN_BLANK)
-    @Email(message = constants.USER_EMAIL_INVALID_FORMATE)
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = constants.USER_EMAIL_INVALID_FORMATE)
     private String email;
 
     @NotEmpty(message = constants.USER_PASSWORD_CAN_NOT_EMPTY)
