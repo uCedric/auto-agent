5a5b3234132eb53838fc4c2cecca055af360514d
Cedric Liu <cedricliu1011@gmail.com>
2024-10-02 17:07:24 +0800

    fix: adjust vector dimention

--- api/src/main/resources/db/migrations/20240927_01.add_pgvector.up.sql
+++ api/src/main/resources/db/migrations/20240927_01.add_pgvector.up.sql
@@ -5,6 +5,6 @@ CREATE EXTENSION IF NOT EXISTS vector;
 -- sentences_vector --
 CREATE TABLE IF NOT EXISTS chunks (
     id SERIAL PRIMARY KEY NOT NULL,
-    vector VECTOR(512) NOT NULL,  -- Adjust dimension as needed
+    vector VECTOR(768) NOT NULL,  -- Adjust dimension as needed
     content TEXT NOT NULL
 );
